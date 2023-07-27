4e0ae0d95f3e41bb294cc9f68540bcc1c93fac17
Ömer Faruk Aslan <112866826+omeraslanw@users.noreply.github.com>
2023-07-27 14:22:27 +0300

    Add project files.

---
+++ Snake_Game_Project/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Snake_Game_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.skorLbl = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(19, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Skor:";
+            // 
+            // skorLbl
+            // 
+            this.skorLbl.AutoSize = true;
+            this.skorLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.skorLbl.Location = new System.Drawing.Point(94, 45);
+            this.skorLbl.Name = "skorLbl";
+            this.skorLbl.Size = new System.Drawing.Size(26, 29);
+            this.skorLbl.TabIndex = 3;
+            this.skorLbl.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Teal;
+            this.panel2.Controls.Add(this.btnStart);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.skorLbl);
+            this.panel2.Location = new System.Drawing.Point(545, 21);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(157, 500);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.Location = new System.Drawing.Point(19, 424);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(113, 46);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Green;
+            this.panel1.Location = new System.Drawing.Point(21, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 500);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(723, 544);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label skorLbl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label btnStart;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Snake_Game_Project/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_Game_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int score = 0;
+        Panel part;
+        Panel apple = new Panel();
+        List<Panel> snake = new List<Panel>();
+        string direction = "right";
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int snakeX = snake[0].Location.X;
+            int snakeY = snake[0].Location.Y;
+
+            skorCheck();
+            removeApple();
+            snakeTail();
+            crash();
+
+            if (direction == "right" && direction != "left")
+            {
+                if (snakeX < 480)
+                    snakeX += 20;
+                else snakeX = 0;
+            }
+            if (direction == "left" && direction != "right")
+            {
+                if (snakeX > 0)
+                    snakeX -= 20;
+                else snakeX = 480;
+            }
+            if (direction == "up" && direction != "down")
+            {
+                if (snakeY > 0)
+                    snakeY -= 20;
+                else snakeY = 480;
+            }
+            if (direction == "down" && direction != "up")
+            {
+                if (snakeY < 480)
+                    snakeY += 20;
+                else snakeY = 0;
+            }
+            snake[0].Location = new Point(snakeX, snakeY);
+        }
+
+        void createApple()
+        {
+            Random appleLoc = new Random();
+            int appleX, appleY;
+            appleX = appleLoc.Next(300);
+            appleY = appleLoc.Next(300);
+
+            appleX -= appleX % 20;
+            appleY -= appleY % 20;
+
+            apple.Size = new Size(20, 20);
+            apple.BackColor = Color.Red;
+            apple.Location = new Point(appleX, appleY);
+            panel1.Controls.Add(apple);
+        }
+        void skorCheck()
+        {
+            switch (score)
+            {
+                case 50: timer1.Interval = 90; break;
+                case 100: timer1.Interval = 80; break;
+                case 150: timer1.Interval = 70; break;
+                case 200: timer1.Interval = 60; break;
+                case 250:timer1.Interval = 50; break;
+            }
+        }
+        void removeApple()
+        {
+
+            if (snake[0].Location == apple.Location)
+            {
+                score += 10;
+                skorLbl.Text = score.ToString();
+                panel1.Controls.Remove(apple);
+                createApple();
+                addPart();
+            }
+        }
+        void addPart()
+        {
+            Panel newpart = new Panel();
+            newpart.Size = new Size(20, 20);
+            newpart.BackColor = Color.DarkGreen;
+            snake.Add(newpart);
+            panel1.Controls.Add(newpart);
+        }
+        void snakeTail()
+        {
+            for (int i = snake.Count - 1; i > 0; i--)
+            {
+                snake[i].Location = snake[i - 1].Location;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int snakeX = snake[0].Location.X;
+            int snakeY = snake[0].Location.Y;
+
+            if (e.KeyCode == Keys.Right)
+                direction = "right";
+
+            if (e.KeyCode == Keys.Left)
+                direction = "left";
+
+            if (e.KeyCode == Keys.Up)
+                direction = "up";
+
+            if (e.KeyCode == Keys.Down)
+                direction = "down";
+
+            snake[0].Location = new Point(snakeX, snakeY);
+        }
+
+        void crash()
+        {
+            for (int i = 2; i < snake.Count; i++)
+            {
+                int score = int.Parse(skorLbl.Text);
+                if (snake[0].Location == snake[i].Location)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Game Over! Score: " + score);
+                }
+            }
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            skorLbl.Text = "0";
+            score= 0;
+            createApple();
+            snake.Clear();
+           
+
+            part = new Panel();
+            part.Location = new Point(200, 200);
+            part.Size = new Size(20, 20);
+            part.BackColor = Color.DarkGreen;
+            snake.Add(part);
+            panel1.Controls.Add(snake[0]);
+
+            timer1.Start();
+
+        }
+    }
+}
